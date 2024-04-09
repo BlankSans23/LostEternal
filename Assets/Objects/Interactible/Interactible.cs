4fcd386c85110764aca3882f5dabf8370c447567
BlankSans23 <mariobros0423@gmail.com>
2024-04-08 20:58:59 -0400

    Its an interface

--- Assets/Objects/Interactible/Interactible.cs
+++ Assets/Objects/Interactible/Interactible.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactible : MonoBehaviour
+public interface Interactible
 {
-
     void Interact(Entity source) { 
     
     }
